8e9c6fefdf15dc6d6a9cd4681984a17d7157714d
bayashi0420 <nobuhiro123456789@gmail.com>
2025-01-23 10:50:26 +0000

    delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,6 +45,11 @@ class PostController extends Controller
         $input_post = $request['post'];
         $post->fill($input_post)->save();
 
-        return redirect('/posts' . $post->id);
+        return redirect('/posts/' . $post->id);
+    }
+    public function delete(post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes as EloquentSoftDeletes;
+use Illuminate\datebase\eloquent\softdeletes;
 
 class Post extends Model
 {
+    use EloquentSoftDeletes;
+
     protected $fillable = [
         "title",
         "body",
